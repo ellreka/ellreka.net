df78f10ae4a29b09777a073877e66621152dc003
ellreka <ellreka5364@gmail.com>
2020-09-21 17:29:15 +0900

    feat: add profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,13 +6,58 @@ import { Title } from '../../components/Title'
 
 const Profile: React.FC = () => {
   const meta = {
-    title: 'プロフィール'
+    title: 'Profile'
   }
   return (
     <Layout>
       <Meta meta={meta} />
       <div className="mx-auto max-w-2xl">
         <Title>Profile</Title>
+        <div className="mt-10">
+          <h2 className="dark:text-white text-lg font-bold mt-10">About</h2>
+          <p className="dark:text-white">something</p>
+          <h2 className="dark:text-white text-lg font-bold mt-10">Hobby</h2>
+          <p className="dark:text-white">
+            Programming, Watching a live streaming, Playing games
+          </p>
+          <h2 className="dark:text-white text-lg font-bold mt-10">Favorite</h2>
+          <p className="dark:text-white">Dvorak, </p>
+          {/* <h2 className="dark:text-white text-lg font-bold mt-10">Skill</h2>
+          <p className="dark:text-white">JavaScript, TypeScript, React,</p> */}
+          <h2 className="dark:text-white text-lg font-bold mt-10">Accounts</h2>
+          <ul className="dark:text-white">
+            <li>
+              <span>Github / </span>
+              <a
+                href="https://github.com/ellreka"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-600">
+                https://github.com/ellreka
+              </a>
+            </li>
+            {/* <li>
+              <span>Twitter / </span>
+              <a
+                href="https://github.com/ellreka"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-600">
+                https://twitter.com/ellreka
+              </a>
+            </li> */}
+            <li>
+              <span>Mail / </span>
+              <a
+                href="mailto:ellreka5364@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-600">
+                ellreka5364@gmail.com
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </Layout>
   )

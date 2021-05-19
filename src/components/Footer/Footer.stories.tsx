8cc71c4e2ce95cd421550336e7ced78bac1784e1
ellreka <ellreka5364@gmail.com>
2021-05-20 04:14:04 +0900

    test1

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -7,7 +7,11 @@ import { Story } from '@storybook/react/types-6-0'
 export default { title: 'Footer' }
 
 export const footer: Story<React.ComponentProps<typeof Footer>> = (props) => {
-  return <Footer />
+  return (
+    <div className="mb-60">
+      <Footer />
+    </div>
+  )
 }
 
 footer.args = {}

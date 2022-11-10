67ea09f559478d222220b61f571ae440e4c52e89
ellreka <ellreka5364@gmail.com>
2022-11-10 12:27:54 +0900

    fix: add viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,8 @@ export default function RootLayout({
   return (
     <html lang="ja" className={mPlus1p.className}>
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <GoogleAnalytics />
       </head>
       <body>

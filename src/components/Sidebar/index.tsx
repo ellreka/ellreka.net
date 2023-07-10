18f98bc19c34a83cad4764a9304bf03150924358
ellreka <ellreka5364@gmail.com>
2023-07-11 03:48:32 +0900

    feat: update

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -32,27 +32,31 @@ export function Sidebar({ headings }: Props): React.ReactElement {
     }
   })
   return (
-    <nav className="sticky top-10 p-3 bg-gray-200">
-      <ul>
-        {list.map((item, idx) => (
-          <li className="mt-3 text-gray-800 text-sm" key={idx}>
-            <a className="hover:text-blue-500" href={`#${item.title}`}>
-              {item.title}
-            </a>
-            {item.children !== null && (
-              <ul className="ml-6">
-                {item.children.map((child, idx) => (
-                  <li className="mt-3" key={idx}>
-                    <a className="hover:text-blue-500" href={`#${child.title}`}>
-                      - {child.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className="sticky top-10 max-h-[calc(100vh-20px)] overflow-auto rounded-md bg-gray-200">
+      <nav className="h-full p-3">
+        <ul>
+          {list.map((item, idx) => (
+            <li className="mt-3 text-sm text-gray-800" key={idx}>
+              <a className="hover:text-blue-500" href={`#${item.title}`}>
+                {item.title}
+              </a>
+              {item.children !== null && (
+                <ul className="ml-6">
+                  {item.children.map((child, idx) => (
+                    <li className="mt-3" key={idx}>
+                      <a
+                        className="hover:text-blue-500"
+                        href={`#${child.title}`}>
+                        - {child.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   )
 }

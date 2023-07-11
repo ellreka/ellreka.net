c5ee3a3dbb159e8890c3540340c1a9e6db7edcf1
ellreka <ellreka5364@gmail.com>
2023-07-12 05:17:34 +0900

    chore: fix gen-mdx.sh

--- bin/gen-mdx.sh
+++ bin/gen-mdx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cat > ./docs/$1.mdx << __EOF__
----
-  date: '$(date '+%Y-%m-%d')'
-  title: ''
+export const meta = {
+  date: '$(date '+%Y-%m-%d')',
+  title: '',
   tags: ['']
----
+}
 
 ## title
 __EOF__

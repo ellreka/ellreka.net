d7efb097783d19588e70838aafa6c7308765238a
ellreka <ellreka5364@gmail.com>
2024-02-09 00:42:50 +0900

    feat: update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 
 export function Footer(): React.ReactElement {
   return (
-    <footer className="mt-5 h-12 text-center">
-      <div className="mx-auto flex w-12 justify-between">
-        <a
-          href="https://github.com/ellreka"
-          target="_blank"
-          rel="noreferrer"
-          className="text-black dark:text-white">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-        <a
-          href="https://twitter.com/ellreka"
-          target="_blank"
-          rel="noreferrer"
-          className="text-black dark:text-white">
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
+    <footer className="mx-auto mt-10 mb-5 w-full max-w-2xl">
+      <div className="flex w-full justify-between text-gray-400">
+        <div className="flex gap-3">
+          <small className="">© ellreka</small>
+          <a
+            href="https://github.com/ellreka"
+            target="_blank"
+            rel="noreferrer"
+            className="">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://twitter.com/ellreka"
+            target="_blank"
+            rel="noreferrer"
+            className="">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
+        <div className="flex gap-3 text-sm">
+          <Link href="/privacy" className="">
+            Privacy Policy
+          </Link>
+        </div>
       </div>
-      <small className="text-black dark:text-white">© ellreka</small>
     </footer>
   )
 }
